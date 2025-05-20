783c27a633070079ee0b430912d4aef90bcf0d40
zhoucheng <zhoucheng133@outlook.com>
2025-05-20 19:45:41 +0800

    添加参数合法性检查

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -2,6 +2,11 @@ import Database from "bun:sqlite";
 import { ResponseBody, ToResponseBody } from "./static";
 import { nanoid } from "nanoid";
 
+interface ListItem{
+  url: string,
+  key: string
+}
+
 export class List{
   add(body: any, db: Database): ResponseBody{
     if (!body || !body.data) {
@@ -9,17 +14,21 @@ export class List{
     }
 
     try {
-      const data: string[]=body.data as string[];
-      data.forEach((element: string) => {
-        const existingItem = db.prepare("SELECT * FROM list WHERE url = ?").get(element);
-        if (existingItem) {
-          return ToResponseBody(false, "已存在的项目");
+      const data: ListItem[]=body.data as ListItem[];
+      for (const element of data) {
+        if(element.key && element.url ){
+          const existingItem = db.prepare("SELECT * FROM list WHERE url = ?").get(element.url);
+          if (existingItem) {
+            return ToResponseBody(false, "已存在的项目");
+          }
+        }else{
+          return ToResponseBody(false, "参数不正确")
         }
-      });
-      data.forEach((item: string)=>{
+      }
+      data.forEach((item: ListItem)=>{
         const id=nanoid();
-        db.prepare("INSERT INTO list (id, url) VALUES (?, ?)")
-          .run(id, item);
+        db.prepare("INSERT INTO list (id, url, key) VALUES (?, ?, ?)")
+          .run(id, item.url, item.key);
       })
       return ToResponseBody(true, "");
     } catch (error) {
@@ -50,8 +59,12 @@ export class List{
     }
 
     try {
-      const data=body.data as string;
-      db.prepare(`UPDATE list SET url = ? WHERE id = ?`).run(data, id);
+      const data=body.data as ListItem;
+      if(data.key && data.url){
+        db.prepare(`UPDATE list SET url = ?, key = ? WHERE id = ?`).run(data.url, data.key, id);
+      }else{
+        return ToResponseBody(false, "参数不正确")
+      }
     } catch (error) {
       return ToResponseBody(false, error);
     }
